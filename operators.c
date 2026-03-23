7bd46e2468a66e90012c9d361172295ecd24939d
Monisha <monishasundaram17@gmail.com>
2026-03-23 15:35:52 +0530

    Implement basic arithmetic operations in operators.c
    Fix syntax errors in operators.c and implement basic arithmetic operations.

---
+++ operators.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int a,b;
+  scanf("%d %d",&a,&b);
+//addition
+int sum = a+b;
+//subdraction
+int diff = a-b;
+//multiplication
+int mul = a*b;
+//division
+int quo = a/b
+  return 0;
+}
