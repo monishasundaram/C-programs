6573f54a78f634da0590a32b232552f415356d6d
Monisha <monishasundaram17@gmail.com>
2026-03-24 09:33:43 +0530

    Relational operators demonstration in C

---
+++ relationalop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  int a = 5;
+  int b = 7;
+  //Equalto operator
+  printf("a == b : %d\n", a == b);
+  //Notequalto operator
+  printf("a != b : %d\n", a != b);
+  //Greater than operator
+  printf("a > b  : %d\n", a > b);
+  //Less than operator
+  printf("a < b  : %d\n", a < b);
+  //Greater than equal to operator
+  printf("a >= b : %d\n", a >= b);
+  //Less than equal to operator
+  printf("a <= b : %d\n", a <= b);
+
+  return 0;
+}
