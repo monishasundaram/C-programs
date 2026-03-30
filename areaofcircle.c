17f788ed2f1e2b39ead4b1914ea45ba615ec4512
Monisha <monishasundaram17@gmail.com>
2026-03-30 14:49:05 +0530

    Add program to calculate area of a circle

---
+++ areaofcircle.c
@@ -0,0 +1,11 @@
+// Area of a circle
+#include <stdio.h>
+
+int main() {
+    float r, area;
+    scanf("%f", &r);
+    // Area of circle formulae
+    area = 3.14 * r * r;
+    printf("Area = %.2f", area);
+    return 0;
+}
